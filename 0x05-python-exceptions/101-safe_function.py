905216191aaa1ed6d5542618e2450d29c38ac010
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2022-12-21 09:04:07 +0300

    Update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 import sys
-
-
-def safe_print_integer_err(value):
-    from sys import stderr
+def safe_function(fct, *args):
     try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=stderr)
-        return False
+        return fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
